c102c4a202e3a9536b516e3e22564eedab7a0bae
Roxedus <me@roxedus.dev>
2022-02-26 23:43:08 +0100

    Bump alpine And patch the breaking change Add var for aiding with further development Tested by updating readmevars

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 if [[ -z "${CI_RUN}" ]]; then
     cd /ansible
-    ansible-playbook generate.yml
+    ansible-playbook generate.yml ${EXTRA_PLAYBOOK_PARAM}
 else
     echo '[services.d] done.'
     python3 /ansible/server.py
