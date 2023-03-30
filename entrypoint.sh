73ffd57cbcfb435f404a4db205a8ce2f0b791b1d
aptalca <541623+aptalca@users.noreply.github.com>
2023-03-30 14:30:46 -0400

    update init done log message for the builder

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,6 @@ if [[ -z "${CI_RUN}" ]]; then
     cd /ansible
     ansible-playbook generate.yml ${EXTRA_PLAYBOOK_PARAM}
 else
-    echo '[services.d] done.'
+    echo '[ls.io-init] done.'
     python3 /ansible/server.py
 fi
