c9e2d1f6d1306553518e4358431a92261802ee7f
Roxedus <me@roxedus.dev>
2025-01-27 11:27:17 +0100

    Restructure files Use catatonit Remove CI server page

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
+cat /build_version
+
 if [[ -z "${CI_RUN}" ]]; then
-    cd /ansible
-    ansible-playbook generate.yml ${EXTRA_PLAYBOOK_PARAM}
+    ansible-playbook generate.yml "$@"
 else
     echo '[ls.io-init] done.'
-    python3 /ansible/server.py
 fi
